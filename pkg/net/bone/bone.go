1213445fdb86ce9bde4aaaabeec57326d475f130
jim_sun <jim_sun@intsig.net>
2020-12-14 16:57:08 +0800

    debug

--- pkg/net/bone/bone.go
+++ pkg/net/bone/bone.go
@@ -3,6 +3,8 @@ package bone
 import (
 	"github.com/zdao-pro/sky_blue/pkg/net/bone/middleware"
 
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,5 +15,8 @@ var Server *gin.Engine
 func Init() *gin.Engine {
 	Server = gin.New()
 	Server.Use(middleware.GetTracer(), middleware.GetLogger(middleware.LogConfig{}))
+	Server.GET("/ping", func(c *gin.Context) {
+		c.AbortWithStatus(http.StatusOK)
+	})
 	return Server
 }
--- pkg/net/bone/bone_test.go
+++ pkg/net/bone/bone_test.go
@@ -1,17 +1,12 @@
 package bone
 
 import (
-	"github.com/zdao-pro/sky_blue/pkg/log"
 	"testing"
+
+	"github.com/zdao-pro/sky_blue/pkg/log"
 
 	"github.com/gin-gonic/gin"
 )
-
-func ping(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "pong",
-	})
-}
 
 func cli(c *gin.Context) {
 	c.JSON(200, gin.H{
@@ -22,7 +17,6 @@ func cli(c *gin.Context) {
 func TestApollo(t *testing.T) {
 	log.Init(nil)
 	e := Init()
-	e.GET("/ping", ping)
 	e.GET("/cli", cli)
 	e.Run()
 }
