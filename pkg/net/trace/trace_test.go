502a6231a648bfc0f98c871a155719788e5205a5
jim_sun <jim_sun@intsig.net>
2021-02-02 16:24:05 +0800

    debug

--- pkg/net/trace/trace_test.go
+++ pkg/net/trace/trace_test.go
@@ -1,25 +1,20 @@
 package trace
 
-import (
-	"testing"
-	"time"
+// "github.com/opentracing/opentracing-go"
 
-	"github.com/opentracing/opentracing-go"
-)
+// func TestZipkin(t *testing.T) {
+// 	Init()
+// 	trace := NewTracer("Main")
+// 	span := trace.StartSpan("zipkin_test")
+// 	time.Sleep(time.Duration(1) * time.Second)
+// 	spa2 := opentracing.StartSpan("zip", opentracing.ChildOf(span.Context()))
+// 	spa3 := opentracing.StartSpan("zip2", opentracing.ChildOf(span.Context()))
 
-func TestZipkin(t *testing.T) {
-	Init()
-	trace := NewTracer("Main")
-	span := trace.StartSpan("zipkin_test")
-	time.Sleep(time.Duration(1) * time.Second)
-	spa2 := opentracing.StartSpan("zip", opentracing.ChildOf(span.Context()))
-	spa3 := opentracing.StartSpan("zip2", opentracing.ChildOf(span.Context()))
+// 	time.Sleep(time.Duration(1) * time.Second)
+// 	spa3.Finish()
+// 	time.Sleep(time.Duration(2) * time.Second)
+// 	spa2.Finish()
+// 	time.Sleep(time.Duration(1) * time.Second)
+// 	span.Finish()
 
-	time.Sleep(time.Duration(1) * time.Second)
-	spa3.Finish()
-	time.Sleep(time.Duration(2) * time.Second)
-	spa2.Finish()
-	time.Sleep(time.Duration(1) * time.Second)
-	span.Finish()
-
-}
+// }
